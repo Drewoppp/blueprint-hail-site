8f3726a7c3f304d6f18a87ae87039b8c3a8963b0
Drewoppp <144189618+Drewoppp@users.noreply.github.com>
2025-08-18 14:29:08 -0500

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -1,9 +1,1 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
 
-navToggle.addEventListener("click", () => {
-  const expanded = navToggle.getAttribute("aria-expanded") === "true" || false;
-  
-  navToggle.setAttribute("aria-expanded", !expanded);
-  nav.classList.toggle("active");
-});
